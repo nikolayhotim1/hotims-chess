d829afa79686b536062f0c6fd7e3d29707a55b2b
nikolayhotim1 <hotim_nikolay@mail.ru>
2022-05-22 15:20:45 +0300

    feature: create and add chess figures on the board

--- src/models/Board.ts
+++ src/models/Board.ts
@@ -1,3 +1,9 @@
+import { Knight } from './figures/Knight';
+import { Rook } from './figures/Rook';
+import { King } from './figures/King';
+import { Bishop } from './figures/Bishop';
+import { Pawn } from './figures/Pawn';
+import { Queen } from './figures/Queen';
 import { Colors } from './Colors';
 import { Cell } from './Cell';
 
@@ -16,5 +22,56 @@ export class Board {
             }
             this.cells.push(row);
         }
+    }
+
+    public getCell(x: number, y: number) {
+        return this.cells[y][x];
+    }
+
+    private addKings() {
+        new King(Colors.BLACK, this.getCell(4, 0));
+        new King(Colors.WHITE, this.getCell(4, 7));
+    }
+
+    private addQueens() {
+        new Queen(Colors.BLACK, this.getCell(3, 0));
+        new Queen(Colors.WHITE, this.getCell(3, 7));
+    }
+
+    private addKnights() {
+        new Knight(Colors.BLACK, this.getCell(1, 0));
+        new Knight(Colors.BLACK, this.getCell(6, 0));
+        new Knight(Colors.WHITE, this.getCell(1, 7));
+        new Knight(Colors.WHITE, this.getCell(6, 7));
+    }
+
+    private addRooks() {
+        new Rook(Colors.BLACK, this.getCell(0, 0));
+        new Rook(Colors.BLACK, this.getCell(7, 0));
+        new Rook(Colors.WHITE, this.getCell(0, 7));
+        new Rook(Colors.WHITE, this.getCell(7, 7));
+    }
+
+    private addBishops() {
+        new Bishop(Colors.BLACK, this.getCell(2, 0));
+        new Bishop(Colors.BLACK, this.getCell(5, 0));
+        new Bishop(Colors.WHITE, this.getCell(2, 7));
+        new Bishop(Colors.WHITE, this.getCell(5, 7));
+    }
+
+    private addPawns() {
+        for (let i = 0; i < 8; i++) {
+            new Pawn(Colors.BLACK, this.getCell(i, 1));
+            new Pawn(Colors.WHITE, this.getCell(i, 6));
+        }
+    }
+
+    public addFigures() {
+        this.addKings();
+        this.addQueens();
+        this.addKnights();
+        this.addRooks();
+        this.addBishops();
+        this.addPawns();
     }
 };
--- src/models/figures/Figure.ts
+++ src/models/figures/Figure.ts
@@ -1,3 +1,36 @@
+import { Cell } from './../Cell';
+import { Colors } from './../Colors';
+import logo from '../../assets/black-king.png';
+
+export enum FigureNames {
+    FIGURE = 'FIGURE',
+    KING = 'KING',
+    QUEEN = 'QUEEN',
+    KNIGHT = 'KNIGHT',
+    ROOK = 'ROOK',
+    BISHOP = 'BISHOP',
+    PAWN = 'PAWN'
+};
+
 export class Figure {
-    
+    color: Colors;
+    logo: typeof logo | null;
+    cell: Cell;
+    name: FigureNames;
+    id: number;
+
+    constructor(color: Colors, cell: Cell) {
+        this.color = color;
+        this.logo = null;
+        this.cell = cell;
+        this.cell.figure = this;
+        this.name = FigureNames.FIGURE;
+        this.id = Math.random();
+    }
+
+    canMovee(target: Cell): boolean {
+        return true;
+    }
+
+    moveFigure(target: Cell) { }
 };
